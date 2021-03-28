eb314a985331d0aba3db888de4c1acff97c5452d
amohammdi <alif.mohamady20@gmail.com>
2021-03-28 12:28:23 +0430

    update

--- components/Main/FullScrollPage.js
+++ components/Main/FullScrollPage.js
@@ -120,22 +120,22 @@ class FullScrollPage {
             document.addEventListener('DOMMouseScroll', handleMouseWheel);
         }
         document.addEventListener('scroll', handleMouseWheel);
-        document.addEventListener('touchstart', (event) => {
-            this.startY = event.touches[0].pageY;
-        });
-        document.addEventListener('touchend', (event) => {
-            let endY = event.changedTouches[0].pageY;
-            if (this.startY - endY < 0) {
-                this.scrollUp();
-            }
-            if (this.startY - endY > 0) {
-                this.scrollDown();
-            }
-        });
+        // document.addEventListener('touchstart', (event) => {
+        //     this.startY = event.touches[0].pageY;
+        // });
+        // document.addEventListener('touchend', (event) => {
+        //     let endY = event.changedTouches[0].pageY;
+        //     if (this.startY - endY < 0) {
+        //         this.scrollUp();
+        //     }
+        //     if (this.startY - endY > 0) {
+        //         this.scrollDown();
+        //     }
+        // });
         // document.addEventListener('touchmove', (event) => {
         //     event.preventDefault();
         // });
-        // window.addEventListener('resize', handleResize);
+        window.addEventListener('resize', handleResize);
     }
 }
 
