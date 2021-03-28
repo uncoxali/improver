d716327f838cca9366064494a20b3cae03ab69f0
amohammdi <alif.mohamady20@gmail.com>
2021-03-28 17:39:38 +0430

    update

---
+++ components/ui/index.ts
@@ -0,0 +1,6 @@
+export { default as Button } from './Button';
+export { default as VerticalList } from './VerticalList';
+export { default as HorizontalList } from './HorizontalList';
+export { default as Modal } from './Modal';
+export { default as Input } from './Input';
+export { default as DropDown } from './Dropdown';
